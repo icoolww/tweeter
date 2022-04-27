81f2e1f3137bc6dd783543eb8c13ad0f444e4428
William <williamwijaya.mail@gmail.com>
2022-04-27 16:28:17 +0000

    adding tweets template and element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,36 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// creating a data template
+const createTweetElement = function (data) {
+  const name = data.user.name;
+  const avatar = data.user.avatars;
+  const handle = data.user.handle;
+  const content = data.content.text;
+  const time = data.created_at;
+
+  const $tweet = $(`
+    <article class="tweet-container">  
+      <label for="testing">Testing Testing</label>
+        <header>
+              
+        </header>
+          <hr />
+        <footer>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </footer>
+    </article>
+    `);
+
+  return $tweet;
+};
+
+  // creating tweet element by usng template
+  const renderTweets = function (datas) {
+    for (let data of datas) {
+      let $tweet = createTweetElement(data);
+      $("#tweets-container").append($tweet);
+    }
+  };
